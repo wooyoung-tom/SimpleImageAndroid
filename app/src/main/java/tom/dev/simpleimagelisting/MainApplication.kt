b32b9bcd8f8d1c446cf8a42b4f9987909566868d
WooYoung Choi <djdj7087@kakao.com>
2021-04-10 15:51:16 +0900

    first commit

---
+++ app/src/main/java/tom/dev/simpleimagelisting/MainApplication.kt
@@ -0,0 +1,10 @@
+package tom.dev.simpleimagelisting
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Simple Application For Hilt
+ */
+@HiltAndroidApp
+class MainApplication : Application() {}
---
+++ app/src/main/java/tom/dev/simpleimagelisting/viewmodel/MainViewModel.kt
@@ -0,0 +1,4 @@
+package tom.dev.simpleimagelisting.viewmodel
+
+class MainViewModel {
+}
