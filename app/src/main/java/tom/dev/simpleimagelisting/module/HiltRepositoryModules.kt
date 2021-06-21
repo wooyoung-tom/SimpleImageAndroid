5bf791ae60567cdf24da30cb9a29695dafdd9145
WooYoung Choi <djdj7087@gmail.com>
2021-06-21 11:23:59 +0900

    gradle version update

--- app/src/main/java/tom/dev/simpleimagelisting/module/HiltRepositoryModules.kt
+++ app/src/main/java/tom/dev/simpleimagelisting/module/HiltRepositoryModules.kt
@@ -3,12 +3,13 @@ package tom.dev.simpleimagelisting.module
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.components.SingletonComponent
 import tom.dev.simpleimagelisting.model.repository.ImageRepository
 import tom.dev.simpleimagelisting.model.repository.ImageRepositoryImpl
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ViewModelComponent::class)
 abstract class HiltRepositoryModules {
 
     @Binds
